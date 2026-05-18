35f7b63c31aacae0e4ce9f0b0d3216314b497d1f
Ian Giurda <iangiurda.dev@gmail.com>
2026-05-18 11:14:07 -0300

    feat(main): implementar notificaciones via mail

--- src/main.py
+++ src/main.py
@@ -1,16 +1,46 @@
 from scraper import consultar_fechas_disponibles
-from storage import cargar_estado, guardar_estado
-from notifier import notificar
+from storage import cargar_estado, guardar_estado, cargar_interesados
+from notifier import enviar_email
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
 
 def main():
+
+    CORREO_REMITENTE = os.getenv("CORREO_REMITENTE")
+    CONTRASENA = os.getenv("CONTRASENA")
+
     fechas = consultar_fechas_disponibles()
     notificadas = cargar_estado()
+    interesados = cargar_interesados()
+
 
     for fecha in fechas:
-        if fecha in notificadas:
+
+        dia = fecha["dia"]
+        id_examen = fecha["id"]
+        ruta_adjunto = fecha["ruta"]
+
+        print(f"id_examen: {id_examen}")
+        print(f"dia: {dia}")
+
+        if id_examen in notificadas:
             continue
 
-        notificar(fecha)
-        notificadas.add(fecha)
+        destinatarios = interesados.get(dia, [])
+
+        for destinatario in destinatarios:
+            enviar_email(CORREO_REMITENTE,
+                        CONTRASENA,
+                        destinatario,
+                        f"Aulas Examen {id_examen}",
+                        "Adjunto la imagen de las aulas :D",
+                        ruta_adjunto)
+
+        notificadas.add(id_examen)
 
     guardar_estado(notificadas)
+
+if __name__ == "__main__":
+    main()
