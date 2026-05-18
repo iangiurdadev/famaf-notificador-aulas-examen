37cf5a2630e711a1ef4dbc62acd66c031883141e
Ian Giurda <iangiurda.dev@gmail.com>
2026-05-17 21:04:39 -0300

    feat(notifier): agregar prototipo de notificador

--- src/notifier.py
+++ src/notifier.py
@@ -1,2 +1,70 @@
-def notificar(fecha):
-    print(f"Notificando: {fecha}")
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+from email.message import EmailMessage
+
+from scraper import consultar_fechas_disponibles
+
+load_dotenv()
+
+def enviar_email(remitente, contrasena,
+                 destinatario, asunto,
+                 contenido, ruta_archivo):
+
+    mensaje = EmailMessage()
+
+    mensaje["From"] = remitente
+    mensaje["To"] = destinatario
+    mensaje["Subject"] = asunto
+
+    mensaje.set_content(contenido)
+
+    with open(ruta_archivo, "rb") as f:
+        datos = f.read()
+
+    mensaje.add_attachment(
+        datos,
+        maintype="image",
+        subtype="png",
+        filename="aula.png"
+    )
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        server.starttls()
+
+        server.login(remitente, contrasena)
+
+        server.send_message(mensaje)
+
+        server.quit()
+
+        print("Correo enviado exitosamente")
+
+    except Exception as e:
+        print(f"Error al enviar correo: {e}")
+
+
+if __name__ == "__main__":
+
+    CORREO_REMITENTE = os.getenv("CORREO_REMITENTE")
+    CONTRASENA = os.getenv("CONTRASENA")
+    CORREO_DESTINATARIO = os.getenv("CORREO_DESTINATARIO")
+
+
+    resultados_fechas = consultar_fechas_disponibles() 
+    if resultados_fechas:
+
+        #Como prueba, mandamos el primero
+        dia = resultados_fechas[0]["dia"]
+        ruta_adjunto = resultados_fechas[0]["ruta"]
+
+        enviar_email(CORREO_REMITENTE,
+                     CONTRASENA,
+                     CORREO_DESTINATARIO,
+                     f"Aulas Examen {dia}",
+                     "Adjunto la imagen de las aulas :D",
+                     ruta_adjunto)
