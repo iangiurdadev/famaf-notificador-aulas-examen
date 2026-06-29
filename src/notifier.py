4aa12dc50af12858e2554255b7d108ce5b97c112
Ian Giurda <iangiurda.dev@gmail.com>
2026-06-29 00:50:59 -0300

    feat: implementa envio mail con copia oculta para respetar privacidad

--- src/notifier.py
+++ src/notifier.py
@@ -1,8 +1,5 @@
 import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
-import os
 from email.message import EmailMessage
 
 from scraper import consultar_fechas_disponibles
@@ -16,7 +13,8 @@ def enviar_email(remitente, contrasena,
     mensaje = EmailMessage()
 
     mensaje["From"] = remitente
-    mensaje["To"] = ", ".join(destinatarios)
+    mensaje["To"] = remitente
+    mensaje["Bcc"] = ", ".join(destinatarios)
     mensaje["Subject"] = asunto
 
     mensaje.set_content(contenido)
@@ -46,25 +44,3 @@ def enviar_email(remitente, contrasena,
 
     except Exception as e:
         print(f"Error al enviar correo: {e}")
-
-
-if __name__ == "__main__":
-
-    CORREO_REMITENTE = os.getenv("CORREO_REMITENTE")
-    CONTRASENA = os.getenv("CONTRASENA")
-    CORREO_DESTINATARIO = os.getenv("CORREO_DESTINATARIO")
-
-
-    resultados_fechas = consultar_fechas_disponibles() 
-    if resultados_fechas:
-
-        #Como prueba, mandamos el primero
-        dia = resultados_fechas[0]["dia"]
-        ruta_adjunto = resultados_fechas[0]["ruta"]
-
-        enviar_email(CORREO_REMITENTE,
-                     CONTRASENA,
-                     CORREO_DESTINATARIO,
-                     f"Aulas Examen {dia}",
-                     "Adjunto la imagen de las aulas :D",
-                     ruta_adjunto)
