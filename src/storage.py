d3be5eb465130c724f8d378472dd3f005c9effcc
Ian Giurda <iangiurda.dev@gmail.com>
2026-06-28 23:35:04 -0300

    feat: implementar persistencia en JSON

--- src/storage.py
+++ src/storage.py
@@ -1,12 +1,24 @@
+import json
+import os
+
+FECHAS_NOTIFICADAS = "notificadas.json"
+
+
 def cargar_estado():
-    return set()
+    if not os.path.exists(FECHAS_NOTIFICADAS):
+        print(f"no existe el archivo {FECHAS_NOTIFICADAS}")
+        return set()
+
+    with open(FECHAS_NOTIFICADAS, "r", encoding="utf-8") as archivo:
+        return set(json.load(archivo))
 
 def guardar_estado(notificados):
-    pass
+    with open(FECHAS_NOTIFICADAS, "w", encoding="utf-8") as archivo:
+        json.dump(list(notificados), archivo)
 
 def cargar_interesados():
     return {
-        "martes": [
+        "lunes": [
             "iangiurda.dev@gmail.com"
         ]
     }
